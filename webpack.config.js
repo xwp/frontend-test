ed46d118e3e3db7f7f4eb74f6eb5be8fb7cc7bec
Piotr Delawski <piotr.delawski@gmail.com>
2021-03-19 18:00:11 +0100

    Install missing deps and format JS with eslint

--- webpack.config.js
+++ webpack.config.js
@@ -13,46 +13,46 @@ const WebpackBar = require( 'webpackbar' );
 /**
  * WordPress dependencies
  */
- const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
 
- const sharedConfig = {
-	 output: {
-		 path: path.join( __dirname, 'js/dist' ),
-		 filename: `[name].js`,
-		 chunkFilename: `[name].js`,
-	 },
-	 optimization: {
-		 minimizer: [
-			 new TerserPlugin( {
-				 parallel: true,
-				 sourceMap: false,
-				 cache: true,
-				 terserOptions: {
-					 output: {
-						 comments: /translators:/i,
-					 },
-				 },
-				 extractComments: false,
-			 } ),
-		 ],
-	 },
-	 plugins: [ ...defaultConfig.plugins ],
- };
+const sharedConfig = {
+	output: {
+		path: path.join( __dirname, 'js/dist' ),
+		filename: `[name].js`,
+		chunkFilename: `[name].js`,
+	},
+	optimization: {
+		minimizer: [
+			new TerserPlugin( {
+				parallel: true,
+				sourceMap: false,
+				cache: true,
+				terserOptions: {
+					output: {
+						comments: /translators:/i,
+					},
+				},
+				extractComments: false,
+			} ),
+		],
+	},
+	plugins: [ ...defaultConfig.plugins ],
+};
 
- const frontEnd = {
-	 ...defaultConfig,
-	 ...sharedConfig,
-	 entry: {
-		 main: [ './js/src/main.js' ],
-	 },
-	 plugins: [
-		 ...sharedConfig.plugins,
-		 // Display nice progress bar while building or watching.
-		 new WebpackBar( {
-			 name: 'Main',
-			 color: '#36f271',
-		 } ),
-	 ],
- };
+const frontEnd = {
+	...defaultConfig,
+	...sharedConfig,
+	entry: {
+		main: [ './js/src/main.js' ],
+	},
+	plugins: [
+		...sharedConfig.plugins,
+		// Display nice progress bar while building or watching.
+		new WebpackBar( {
+			name: 'Main',
+			color: '#36f271',
+		} ),
+	],
+};
 
- module.exports = [ frontEnd ];
+module.exports = [ frontEnd ];
